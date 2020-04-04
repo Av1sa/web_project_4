01fa24905eaf54193e76f97723d1c02529e36946
Avisa <manelis1102@gmail.com>
2020-04-04 22:43:15 +0300

    Ready for Review 1

---
+++ README.md
@@ -0,0 +1,12 @@
+# Project 4: Around The U.S.
+
+It's a practice project for Yandex Practicum Web Developer course. 
+
+Stack: HTML, CSS (BEM), JS
+Responsive design
+
+You can edit the name and the description in the profile section.
+
+Github Pages: 
+
+
---
+++ js/script.js
@@ -0,0 +1,34 @@
+//Button variables
+let editButton = document.querySelector('.button_edit');
+let closeButton = document.querySelector('.button_close');
+
+//Profile fields
+let name = document.querySelector('.profile__name');
+let job = document.querySelector('.profile__description');
+
+//Popup fields
+let popup = document.querySelector('.popup');
+let form = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__input_name');
+let jobInput = document.querySelector('.popup__input_job');
+
+
+editButton.addEventListener('click', function(){
+  popup.classList.add('popup_opened');
+  nameInput.value = name.textContent;
+  jobInput.value = job.textContent;
+});
+
+closeButton.addEventListener('click', function(){
+  popup.classList.remove('popup_opened');
+});
+
+form.addEventListener('submit', function(e){
+  if (nameInput.value !== '' && jobInput.value !== '') {
+    name.textContent = nameInput.value;
+    job.textContent = jobInput.value
+  }
+  e.preventDefault();
+});
+
+
